62800ff177d2bcf3da3d384f8076aabbf6a09d06
Ubuntu <zjing@umail.ucsb.edu>
2021-02-25 03:11:18 +0000

    Updated...

--- run_DESeq2.py
+++ run_DESeq2.py
@@ -18,7 +18,7 @@ def main():
     ret_r = r['run_DESeq'](assay_mat, phe_vec)
     ret_r_as_df = r['as.data.frame'](ret_r)
 
-    ret_py_df = pandas2ri.ri2py_dataframe(ret_r_as_df)
+    ret_py_df = pandas2ri.ri2py(ret_r_as_df)
     # TODO: maybe rename the columns to be more self-explanatory?
     result_df = ret_py_df
     result_df = result_df.sort_values('padj')
